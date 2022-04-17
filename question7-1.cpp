73fc05b97f649100eb943b8ac8483180d223a5c4
Denny0411 <101331755+Denny0411@users.noreply.github.com>
2022-04-17 21:01:01 +0800

    Create question7-1.cpp

---
+++ question7-1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+void judge(int);
+
+int main()
+{
+    int num;
+    cout << "請輸入一整數: ";
+    cin >> num;
+    judge(num);
+
+    return 0;
+}
+
+void judge(int num)
+{
+    int fromOneToNum = 1, divisorNum = 0;
+    while(fromOneToNum <= num)
+    {
+        if(num % fromOneToNum == 0)
+        {
+            cout << fromOneToNum << endl;
+            divisorNum++;
+        }
+        fromOneToNum++;
+    }
+    if(divisorNum == 2)
+    {
+        cout << "質數";
+    }
+}
